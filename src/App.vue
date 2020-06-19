23e6e66b50495c3b21ab6bd87bfc15f4514184ac
Jehan <gaozhiheng_email@163.com>
2020-06-19 14:11:57 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="testing 00000!!!"/>
+    <HelloWorld msg="testing 哈哈哈222!!!"/>
   </div>
 </template>
 
