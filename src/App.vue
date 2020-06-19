9e5bb3bfc227a9ecd8bf84a2d2502c8541cc2197
Jehan <gaozhiheng_email@163.com>
2020-06-19 15:07:18 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="test deployed888!!!"/>
+    <HelloWorld msg="test deployed???!!!"/>
   </div>
 </template>
 
