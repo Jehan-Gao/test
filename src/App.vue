64748c5cac9c31954ab8237daa743374e383497b
Jehan <gaozhiheng_email@163.com>
2020-07-20 11:08:23 +0800

    fix bug

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="test deployed???!!!"/>
+    <HelloWorld msg="test deployed???!!!11113555"/>
   </div>
 </template>
 
